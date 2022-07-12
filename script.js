60d50e017f90cde85da92a7e444441ac1271f0c3
Jake Porter <Jake.m.porter@gmail.com>
2022-07-11 20:34:05 -0500

    trying to reduce timer with wrong answer

--- script.js
+++ script.js
@@ -16,13 +16,9 @@ var questionThreeAnswers = ["Numbers and Strings", "Other Arrays", "Booleans", "
 var questionFourAnswers = ["Commas", "Curly Brackets", "Quotes", "Parenthesis"];
 var questionFiveAnswers = [];
 
-questionAsked.text(questions[0]);
-
-answerOptions.children("#Answer-One").text();
-
+var secondsLeft = 60;
 function countdown() {
   var timeEl = rootEl.children("header").children("#Timer");
-  var secondsLeft = 60;
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.text(secondsLeft + " seconds left.");
@@ -31,7 +27,27 @@ function countdown() {
       clearInterval(timerInterval);
     }
   }, 1000);
+  questionOne();
 }
 var startGame = rootEl.children("section").children("#Start-Game");
 
 startGame.on("click", countdown);
+function wrongAnswer() {
+  secondsLeft - 10;
+}
+
+function questionOne() {
+  if (secondsLeft > 0) {
+    questionAsked.text(questions[0]);
+    answerOptions.children("#Answer-One").children("button").text(questionOneAnswers[0]);
+    answerOptions.children("#Answer-Two").children("button").text(questionOneAnswers[1]);
+    answerOptions.children("#Answer-Three").children("button").text(questionOneAnswers[2]);
+    answerOptions.children("#Answer-Four").children("button").text(questionOneAnswers[3]);
+  }
+  var answerOne = answerOptions.children("#Answer-One").children("button");
+  var answerTwo = answerOptions.children("#Answer-Two").children("button");
+  var answerThree = answerOptions.children("#Answer-Three").children("button");
+  var answerFour = answerOptions.children("#Answer-Four").children("button");
+  answerOne.on("click", wrongAnswer);
+}
+function questionTwo() {}
